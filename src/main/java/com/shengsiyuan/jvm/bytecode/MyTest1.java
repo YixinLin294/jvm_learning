5f1a4750cee0e5b3d5978a64931eb5b2d70c8a61
alexlin <alexlin@shenlanbao.com>
2020-06-15 00:30:06 +0800

    garbage collector

--- src/main/java/com/shengsiyuan/jvm/bytecode/MyTest1.java
+++ src/main/java/com/shengsiyuan/jvm/bytecode/MyTest1.java
@@ -1,11 +1,28 @@
 package com.shengsiyuan.jvm.bytecode;
+/*
 
 public class MyTest1 {
-    private int a = 1;
+    private  int a = 1;
     public int getA() {
         return a;
     }
-    public void setA(int a) {
+*/
+/*    public void setA(int a) {
         this.a = a;
+    }*//*
+
+}
+*/
+
+public class MyTest1 {
+    public static void main(String []args) {
+        String str1 = "abc";
+        String str2 = new String("def");
+        String str3 = "abc";
+        String str4 = str2.intern();
+        String str5 = "def";
+        System.out.println(str1 == str3);//true
+        System.out.println(str2 == str4);//false
+        System.out.println(str4 == str5);//true
     }
 }
